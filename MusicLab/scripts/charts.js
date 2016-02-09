46814d9a0a0c5aa1ab7564ae8ce6e51b9155bed0
Anthony Nguyen <resolutedreamer@live.com>
2016-02-09 14:45:02 -0800

    Working Charts

--- MusicLab/scripts/charts.js
+++ MusicLab/scripts/charts.js
@@ -2,12 +2,22 @@
 google.charts.load('current', { 'packages': ['corechart'] });
 
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
+//google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(draw_volume_chart);
+
+google.charts.setOnLoadCallback(draw_motor_pause_chart);
+google.charts.setOnLoadCallback(draw_glass_distance_chart);
+google.charts.setOnLoadCallback(draw_light_lux_chart);
+google.charts.setOnLoadCallback(draw_system_start_chart);
+google.charts.setOnLoadCallback(draw_system_pause_chart);
+google.charts.setOnLoadCallback(draw_track_number_chart);
+
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
 function drawChart() {
+
 
     // Create the data table.
     var data = new google.visualization.DataTable();
@@ -29,6 +39,232 @@ function drawChart() {
     };
 
     // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
     chart.draw(data, options);
 }
+
+function draw_volume_chart() {
+
+    get_sensor_data(function (output) {
+        // here you use the output
+        var volume_data = output[0];
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Volume');
+        for (i = 0; i < 322; i++) {
+            data.addRows([
+                [i, volume_data[i]]
+            ]);
+        }
+
+        // Set chart options
+        var options = {
+            'title': 'Volume',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('volume_chart_div'));
+        chart.draw(data, options);
+    });
+
+
+
+}
+
+
+
+function draw_motor_pause_chart() {
+
+    get_sensor_data(function (output) {
+        var motor_data = output[4];
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Motor Pause');
+
+        for (i = 0; i < 322; i++) {
+            data.addRows([
+                [i, motor_data[i]]
+            ]);
+        }
+
+        // Set chart options
+        var options = {
+            'title': 'Motor Pause',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('motor_pause_chart_div'));
+        chart.draw(data, options);
+    });
+}
+
+
+
+function draw_glass_distance_chart() {
+
+    get_sensor_data(function (output) {
+        // here you use the output
+        var volume_data = output[6];
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Glass Distance');
+        for (i = 0; i < 322; i++) {
+            data.addRows([
+                [i, volume_data[i]]
+            ]);
+        }
+
+        // Set chart options
+        var options = {
+            'title': 'Glass Distance',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('glass_distance_chart_div'));
+        chart.draw(data, options);
+    });
+
+
+}
+
+
+
+function draw_light_lux_chart() {
+
+    get_sensor_data(function (output) {
+        // here you use the output
+        var volume_data = output[5];
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Light Lux');
+        for (i = 0; i < 322; i++) {
+            data.addRows([
+                [i, volume_data[i]]
+            ]);
+        }
+
+        // Set chart options
+        var options = {
+            'title': 'Light Lux',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('light_lux_chart_div'));
+        chart.draw(data, options);
+    });
+
+}
+
+
+
+function draw_system_start_chart() {
+
+    get_sensor_data(function (output) {
+        // here you use the output
+        var volume_data = output[2];
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'System Start');
+        for (i = 0; i < 322; i++) {
+            data.addRows([
+                [i, volume_data[i]]
+            ]);
+        }
+
+        // Set chart options
+        var options = {
+            'title': 'System Start',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('system_start_chart_div'));
+        chart.draw(data, options);
+    });
+
+}
+
+
+
+function draw_system_pause_chart() {
+
+    get_sensor_data(function (output) {
+        // here you use the output
+        var volume_data = output[3];
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'System Pause');
+        for (i = 0; i < 322; i++) {
+            data.addRows([
+                [i, volume_data[i]]
+            ]);
+        }
+
+        // Set chart options
+        var options = {
+            'title': 'System Pause',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('system_pause_chart_div'));
+        chart.draw(data, options);
+    });
+
+}
+
+
+
+function draw_track_number_chart() {
+
+
+    get_sensor_data(function (output) {
+        // here you use the output
+        var volume_data = output[1];
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Track Number');
+        for (i = 0; i < 322; i++) {
+            data.addRows([
+                [i, volume_data[i]]
+            ]);
+        }
+
+        // Set chart options
+        var options = {
+            'title': 'Track Number',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('track_number_chart_div'));
+        chart.draw(data, options);
+    });
+
+}
+
--- MusicLab/scripts/mongo_rest.js
+++ MusicLab/scripts/mongo_rest.js
@@ -1,19 +1,44 @@
 function get_actuator_data() {
     $.ajax({
-        //url: "https://api.mongolab.com/api/1/databases/time_sync_project/collections/light_sensor_data1?q={'_id': {$gt: ".concat(lsd1_last_id, "}}&apiKey=Ya1oggJiF9qEsZfUExiS2OGHUW52tEvP"),
         url: "https://api.mongolab.com/api/1/databases/musiclab/collections/control_play?apiKey=DjDKE7adIsCNxYsls6ItXSR67DquF-11",
         dataType: "json",
         success: function (data) {
             console.log("Data from 'control_play' collection");
-            console.log(data);
+            var real_data = data[0].command_value;
+            if (real_data == 0) {
+                console.log("Not Playing Music");
+                $('#playback_control').prop('checked', false);
+            }
+            else if (real_data == 1) {
+                console.log("Playing Music");
+                $('#playback_control').prop('checked', true);
+            }
         }
     })
+
     $.ajax({
         url: "https://api.mongolab.com/api/1/databases/musiclab/collections/music?apiKey=DjDKE7adIsCNxYsls6ItXSR67DquF-11",
         dataType: "json",
         success: function (data) {
             console.log("Data from 'music' collection");
-            console.log(data);
+            var real_data = data[0].track_id;
+            if (real_data == 1) {
+                console.log("Playing Jingle Bells");
+                $('#op1').prop('selected', true);
+            }
+            else if (real_data == 2) {
+                console.log("Playing Starcraft");
+                $('#op2').prop('selected', true);
+            }
+            else if (real_data == 3) {
+                console.log("Playing Mega Man");
+                $('#op3').prop('selected', true);
+            }
+            else if (real_data == 4) {
+                console.log("Playing Airplane");
+                $('#op4').prop('selected', true);
+            }
+            //$("#target4").html("<i>#target4</i>");
         }
     })
 };
@@ -21,22 +46,84 @@ function get_actuator_data() {
 get_actuator_data();
 
 
-function get_sensor_data() {
+function get_sensor_data(handleData) {
+    console.log("Entered Function get_sensor_data")
+    var my_data = [];
     $.ajax({
         url: "https://api.mongolab.com/api/1/databases/musiclab/collections/sensor_data?apiKey=DjDKE7adIsCNxYsls6ItXSR67DquF-11",
         dataType: "json",
         success: function (data) {
+            console.log("get_sensor_data ajax success")
             console.log("Data from 'sensor_data' collection");
             console.log("Expected 322 Sensor Values");
             console.log(data);
+            console.log(data.length);
+            console.log("Let's get the most recent value");
+            
+            var glass_distance_array = [];
+            var light_lux_array = [];
+            var motor_pause_array = [];
+            var system_pause_array = [];
+            var system_start_array = [];
+            var track_number_array = [];
+            var volume_array = [];
+
+            for (i = 0; i < data.length; i++) {
+                var x = data[i];
+                //console.log(x);
+                glass_distance_array.push(x.glass_distance);
+                light_lux_array.push(x.light_lux);
+                motor_pause_array.push(x.motor_pause);
+                system_pause_array.push(x.system_pause);
+                system_start_array.push(x.system_start);
+                track_number_array.push(x.track_number);
+                volume_array.push(x.volume);
+            }
+
+            console.log(glass_distance_array);
+            console.log(light_lux_array);
+            console.log(motor_pause_array);
+            console.log(system_pause_array);
+            console.log(system_start_array);
+            console.log(track_number_array);
+            console.log(volume_array);
+            
+            /*
+            my_data.push(glass_distance_array);
+            my_data.push(light_lux_array);
+            my_data.push(motor_pause_array);
+            my_data.push(system_pause_array);
+            my_data.push(system_start_array);
+            my_data.push(track_number_array);
+            my_data.push(volume_array);
+            */
+
+            var newest_data = data[0];
+            $("#volume_value").html(newest_data.volume);
+            $("#motor_pause_value").html(newest_data.motor_pause);
+            $("#glass_distance_value").html(newest_data.glass_distance);
+            $("#light_lux_value").html(newest_data.light_lux);
+            $("#system_start_value").html(newest_data.system_start);
+            $("#system_pause_value").html(newest_data.system_pause);
+            $("#track_number_value").html(newest_data.track_number);
+
+            var my_data = [volume_array, track_number_array, system_start_array, system_pause_array, motor_pause_array, light_lux_array, glass_distance_array];
+            
+            handleData(my_data);
+            return my_data;
+        },
+        complete: function () {
+            return my_data;
+            setTimeout(poll_lsd1_data, 5000);
         }
     })
+    return my_data;
 };
 
 get_sensor_data();
 
 
-$(document).ready(function ()
+$(document).ready(function()
 {
 
     $( "#playback_control" ).click(function()
